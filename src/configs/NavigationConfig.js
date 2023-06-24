e815f09d2c98e5205e70ea8b8f1a1595d8993ada
alex8023 <alex8023@yandex.ru>
2023-06-24 10:09:23 +0300

    add user-list page

--- src/configs/NavigationConfig.js
+++ src/configs/NavigationConfig.js
@@ -1,28 +1,52 @@
-import { DashboardOutlined } from '@ant-design/icons';
-import { APP_PREFIX_PATH } from 'configs/AppConfig'
+import { DashboardOutlined, UserOutlined } from '@ant-design/icons';
+import { APP_PREFIX_PATH } from 'configs/AppConfig';
 
+const dashBoardNavTree = [
+  {
+    key: 'main',
+    path: `${APP_PREFIX_PATH}/main`,
+    title: 'sidenav.main',
+    icon: DashboardOutlined,
+    breadcrumb: false,
+    isGroupTitle: true,
+    submenu: [
+      {
+        key: 'dashboards-default',
+        path: `${APP_PREFIX_PATH}/main/dashboards`,
+        title: 'sidenav.main.dashboard',
+        icon: DashboardOutlined,
+        breadcrumb: false,
+        submenu: [],
+      },
+      {
+        key: 'clients',
+        path: `${APP_PREFIX_PATH}/main/clients`,
+        title: 'sidenav.main.clients',
+        icon: UserOutlined,
+        breadcrumb: false,
+        submenu: [
+          {
+            key: 'clients-list',
+            path: `${APP_PREFIX_PATH}/main/clients-list`,
+            title: 'sidenav.main.clients.list',
+            icon: '',
+            breadcrumb: false,
+            submenu: [],
+          },
+          {
+            key: 'clients-group',
+            path: `${APP_PREFIX_PATH}/main/clients-group`,
+            title: 'sidenav.main.clients.group',
+            icon: '',
+            breadcrumb: false,
+            submenu: [],
+          },
+        ],
+      },
+    ],
+  },
+];
 
-const dashBoardNavTree = [{
-  key: 'dashboards',
-  path: `${APP_PREFIX_PATH}/dashboards`,
-  title: 'sidenav.dashboard',
-  icon: DashboardOutlined,
-  breadcrumb: false,
-  isGroupTitle: true,
-  submenu: [
-    {
-      key: 'dashboards-default',
-      path: `${APP_PREFIX_PATH}/dashboards/default`,
-      title: 'sidenav.dashboard.default',
-      icon: DashboardOutlined,
-      breadcrumb: false,
-      submenu: []
-    }
-  ]
-}]
-
-const navigationConfig = [
-  ...dashBoardNavTree
-]
+const navigationConfig = [...dashBoardNavTree];
 
 export default navigationConfig;
---
+++ src/views/app-views/main/clients/clients-list/index-2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const DefaultDashboard = () => {
+  return <>Clients-list</>;
+};
+
+export default DefaultDashboard;
