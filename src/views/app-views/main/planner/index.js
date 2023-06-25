2013204231c82267941e2b752de940cbe6df271e
alex8023 <alex8023@yandex.ru>
2023-06-25 14:13:31 +0300

    add grag furniture

--- src/views/app-views/main/planner/index.js
+++ src/views/app-views/main/planner/index.js
@@ -7,13 +7,8 @@ import './styles.css';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-
-// const furnitures = [
-//   { id: 1, src: armchair, isSelected: false },
-//   { id: 2, src: bed, isSelected: false },
-//   { id: 3, src: sofa2, isSelected: false },
-//   { id: 4, src: sofa3 },
-// ];
+import { nanoid } from '@reduxjs/toolkit';
+import { produce } from 'immer';
 
 export function Planner() {
   const [furnitures, setFurnitures] = useState([
@@ -35,6 +30,7 @@ export function Planner() {
               return (
                 <li
                   className='li'
+                  key={id}
                   onClick={(e) => {
                     console.log(id);
                     setFurnitures(
@@ -57,7 +53,14 @@ export function Planner() {
           sx={{ height: 60, marginRight: 2 }}
           onClick={(e) => {
             setFurnOnPlan(
-              furnOnPlan.concat([furnitures.find((item) => item.isSelected === true)])
+              furnOnPlan.concat([
+                {
+                  ...furnitures.find((item) => item.isSelected === true),
+                  left: 500,
+                  top: 200,
+                  id: nanoid(),
+                },
+              ])
             );
           }}
         >
@@ -65,17 +68,32 @@ export function Planner() {
           {'>'}{' '}
         </Button>
         <Paper elevation={5} sx={{ height: 600, width: 800 }}>
-          <div>
-            {furnOnPlan.map((item) => (
-              <img
-                src={item.src}
-                alt=''
-                onDrag={(e) => {
-                  console.log(e.pageX, e.pageY);
-                }}
-              />
-            ))}
-          </div>
+          {/* <div> */}
+          {furnOnPlan.map((item) => (
+            <img
+              key={item.id}
+              style={{
+                position: 'absolute',
+                left: `${item.left}px`,
+                top: `${item.top}px`,
+              }}
+              draggable={true}
+              src={item.src}
+              alt=''
+              onDrag={(e) => {
+                console.log(e.pageX, e.pageY);
+                if (e.pageX < 1) return;
+                setFurnOnPlan(
+                  produce((draft) => {
+                    const furn = draft.find((item2) => item2.id === item.id);
+                    furn.left = e.pageX;
+                    furn.top = e.pageY;
+                  })
+                );
+              }}
+            />
+          ))}
+          {/* </div> */}
         </Paper>
       </Box>
     </>
